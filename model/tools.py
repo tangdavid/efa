cc80f2accb19ce79f577081606b0e3142bb5d685
tangdavid <tang.david.2000@gmail.com>
2022-06-23 02:21:01 -0500

    random effects

--- model/tools.py
+++ model/tools.py
@@ -1,9 +1,12 @@
 import numpy as np
 from scipy import linalg, stats
+from scipy.linalg import khatri_rao
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn import decomposition
 from tqdm.notebook import tqdm, trange
+
+import rpy2.robjects as robjects
 import time
 import itertools
 import torch
@@ -86,3 +89,46 @@ class tools:
         mainEffect = torch.sum(G @ pathways, dim=1, keepdims=True)
         penalty = reg * torch.sum(torch.abs(pathways))
         loss = torch.norm(Y - mainEffect - interEffect)**2
+
+    
+
+class randomEffects:
+    def np2matrix(x):
+        nr, nc = x.shape
+        xvec = robjects.FloatVector(x.transpose().reshape((x.size)))
+        xr = robjects.r.matrix(xvec, nrow=nr, ncol=nc)
+        return xr
+
+    def aiML(data):
+        np2matrix = randomEffects.np2matrix
+        blup = randomEffects.blup
+
+        robjects.r['source']('./reml_func.R')
+
+        aiML_r = robjects.globalenv['aiML']
+
+        G = data.geno
+        K = 1/data.m * (data.geno @ data.geno.T)
+
+        Kr = robjects.ListVector({'1': np2matrix(K), '2': np2matrix(K*K)})
+        yr = np2matrix(data.pheno)
+        Var = robjects.IntVector([.5, .5])
+
+
+        res = aiML_r(A = Kr, y = yr, verbose=False)
+        var = np.array(res.rx2('Var')).reshape(-1, )
+        V = var[0] * K + var[1] * K * K + var[2] * np.eye(K.shape[0])
+
+        beta, omega = blup(data, var, V)
+        return (beta, omega, var)
+
+    def blup(data, var, V):
+        G = data.geno
+        Y = data.pheno
+        GG = khatri_rao(G.T, G.T).T
+        m_G = G.shape[1]
+        m_GG = GG.shape[1]
+        beta = (var[0]/m_G * G).T @ np.linalg.inv(V) @ Y
+        omega = (var[1]/m_GG * GG).T @ np.linalg.inv(V) @ Y
+
+        return (beta, omega)
