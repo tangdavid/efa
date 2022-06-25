7b31311a27dd6e4dfd66d8fcecd1255241b09a6d
David Tang <tang.david.2000@gmail.com>
2022-06-25 18:58:31 -0500

    simulation code with random effects

--- sim/generate_data.py
+++ sim/generate_data.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.insert(0,'../model')
 
-from models import *
-from tools import *
 from datasets import *
+import numpy as np
+import pickle as pkl
 import argparse
 import csv
 
@@ -14,38 +14,51 @@ if __name__=="__main__":
     parser.add_argument("repeat", help="index of repeat", type=int)
     parser.add_argument("seed", help="seed for rng", type=int)
     parser.add_argument("param", help="which param to benchmark", type=str)
+
+    parser.add_argument('--self', default=True, action='store_true')
+    parser.add_argument('--no-self', dest='self', action='store_false')
+
     args = parser.parse_args()
 
     k = args.ktrue
     repeat = args.repeat
+    self = args.self
+    N = 1000
 
     suffix = '_repeat%d' % (repeat)
-    prefix = './%s_k%d/' % (args.param, k)
+
+    if self:
+        prefix = './%s_k%d_self/' % (args.param, k)
+    else:
+        prefix = './%s_k%d/' % (args.param, k)
 
     oos = {}
     data = {}
 
     np.random.seed(args.seed)
 
-    if args.param in ['percent_uncoordinated', 'anchor_strength']:
-        param_list = np.linspace(0, 0.9, 10).round(1)
-    else:
-        param_list = np.linspace(0.1, 1, 10).round(1)
+    #if args.param in ['percent_uncoordinated', 'anchor_strength']:
+    #    param_list = np.linspace(0, 0.9, 10).round(1)
+    #else:
+    #    param_list = np.linspace(0.1, 1, 10).round(1)
+    
+    param_list = np.linspace(0, 1, 11).round(1)
 
     for param in param_list:
+        p=1e-8 if param==0 else param
         if args.param == 'percent_uncoordinated':
-            data[param] = SimDataset(10000, 20, k=k, noise_omega=param)
+            data[param] = SimDataset(N, 20, k=k, h2=0.5, noise_omega=p, self_interactions=self)
         elif args.param == 'heritability':
-            data[param] = SimDataset(10000, 20, k=k, h2=param)
+            data[param] = SimDataset(N, 20, k=k, h2=p, self_interactions=self)
         elif args.param == 'additive_var':
-            data[param] = SimDataset(10000, 20, k=k, additive_model_var=param)
+            data[param] = SimDataset(N, 20, k=k, h2=0.5, additive_model_var=p, self_interactions=self)
         elif args.param == 'anchor_strength':
-            data[param] = SimDataset(10000, 20, k=k, anchor_strength=param)
+            data[param] = SimDataset(N, 20, k=k, h2=0.5, anchor_strength=p, self_interactions=self)
         else: 
             print('poorly specified parameter')
             exit()
 
-        oos[param] = generateOOS(data[param], 10000)
+        oos[param] = generateOOS(data[param], N)
         
     with open(prefix + 'training' + suffix + '.pkl', 'wb') as f:
         pkl.dump(data, f)
