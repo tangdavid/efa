09959c26b7a8aeab098731705f32735fc3feaf6a
tangdavid <tang.david.2000@gmail.com>
2022-09-14 18:25:36 -0400

    switch perm tests

--- model/datasets.py
+++ model/datasets.py
@@ -156,8 +156,8 @@ class SimDataset:
         lr = LinearRegression()
         lr.fit(self.geno, self.pheno)
         res = self.pheno - lr.predict(self.geno)
-        res = np.random.permutation(res)
-        self.pheno = lr.predict(self.geno) + res
+        self.pheno = np.random.permutation(lr.predict(self.geno)) + res
+
 
 
 class SimDatasetLD:
@@ -210,8 +210,7 @@ class RealDataset:
         lr = LinearRegression()
         lr.fit(self.geno, self.pheno)
         res = self.pheno - lr.predict(self.geno)
-        res = np.random.permutation(res)
-        self.pheno = lr.predict(self.geno) + res
+        self.pheno = np.random.permutation(lr.predict(self.geno)) + res
 
 def splitTrain(data):
     train_G, test_G, train_Y, test_Y = train_test_split(data.geno, data.pheno, test_size=0.2)
